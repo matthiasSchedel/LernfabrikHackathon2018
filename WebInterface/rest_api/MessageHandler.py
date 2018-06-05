00d106fa792660937fd9105f88f2698bc7677469
Matthias Schedel <matthias@Matthiass-MacBook-Pro-2.local>
2018-06-05 10:07:08 +0200

    Message api updated

---
+++ WebInterface/rest_api/MessageHandler.py
@@ -0,0 +1,15 @@
+
+
+class MessageHandler(object):
+    def __init__(self):
+        self.clients = []
+
+    @classmethod
+    def updateClients(self):
+            
+            return "test"
+
+    @classmethod
+    def updateClientsWithSelection(self,selection):
+           
+            return "test"
--- WebInterface/rest_api/api.py
+++ WebInterface/rest_api/api.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from SparqlWrapper import SparqlWrapper
+from MessageHandler import MessageHandler
 
 app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
 sw = SparqlWrapper()
+mh = MessageHandler()
 FUSEKI = '172.18.0.1'  
 
+class UpdateClients(Resource):
+    def post(self):
+        # parser.add_argument('data')
+        parser.add_argument('author')
+        args = parser.parse_args()
+        update = getattr(mh,'updateClients')
+        result = update()
+        return result 
 class SensorInsert(Resource):
     def post(self):
         # parser.add_argument('data')
@@ -15,6 +25,10 @@ class SensorInsert(Resource):
         insert = getattr(sw,'insert')
         result = insert(FUSEKI)
         return result 
+
+class Test(Resource):
+    def get(self):
+        return {"test":"a"} 
 
 class SensorData(Resource):
     def post(self):
@@ -28,6 +42,8 @@ class SensorData(Resource):
 # Create routes
 api.add_resource(SensorData, '/sensor')
 api.add_resource(SensorInsert, '/insert')
+api.add_resource(Test, '/test')
+api.add_resource(UpdateClients, '/updateClients')
 
 # Run the application
 if __name__ == '__main__':
